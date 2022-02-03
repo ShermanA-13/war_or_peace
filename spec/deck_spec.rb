f0839706eefe9395f9a356bb5f8b8319358da350
Sherman Au <shermau13@gmail.com>
2022-02-03 13:08:59 -0700

    Refractor deck_spec.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -9,7 +9,9 @@ describe Deck do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
+
       cards = [card1, card2, card3]
+
       deck = Deck.new(cards)
 
       expect(deck.cards).to eq([card1, card2, card3])
@@ -19,7 +21,9 @@ describe Deck do
          card1 = Card.new(:diamond, 'Queen', 12)
          card2 = Card.new(:spade, '3', 3)
          card3 = Card.new(:heart, 'Ace', 14)
+
          cards = [card1, card2, card3]
+
          deck = Deck.new(cards)
 
       expect(deck.rank_of_card_at(0)).to eq(12)
@@ -31,7 +35,9 @@ describe Deck do
      card1 = Card.new(:diamond, 'Queen', 12)
      card2 = Card.new(:spade, '3', 3)
      card3 = Card.new(:heart, 'Ace', 14)
+
      cards = [card1, card2, card3]
+
      deck = Deck.new(cards)
 
      expect(deck.high_ranking_cards).to eq([card1, card3])
@@ -41,7 +47,9 @@ describe Deck do
          card1 = Card.new(:diamond, 'Queen', 12)
          card2 = Card.new(:spade, '3', 3)
          card3 = Card.new(:heart, 'Ace', 14)
+
          cards = [card1, card2, card3]
+
          deck = Deck.new(cards)
 
     expect(deck.percent_high_ranking).to eq(66.67)
