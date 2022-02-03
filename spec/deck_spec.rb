21ce39e07a0d14b4e5f9fe6b16f7fad23fc0050b
Sherman Au <shermau13@gmail.com>
2022-02-03 09:41:11 -0700

    Successful rspec of holding cards.

---
+++ spec/deck_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require 'pry'
+
+describe Deck do
+
+  it 'holds cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.cards).to eq([card1, card2, card3])
+binding.pry
+    end
+
+
+end
