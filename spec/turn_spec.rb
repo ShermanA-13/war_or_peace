4b55ac30f3bef788bfd6eec6bfb60174946a11c2
Sherman Au <shermau13@gmail.com>
2022-02-04 00:08:20 -0700

    Add log

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -14,14 +14,29 @@ describe Turn do
   card6 = Card.new(:diamond, 'Queen', 12)
   card7 = Card.new(:heart, '3', 3)
   card8 = Card.new(:diamond, '2', 2)
+  card9 = Card.new(:heart, 'Jack', 11)
+  card10 = Card.new(:heart, '10', 10)
+  card11 = Card.new(:heart, '9', 9)
+  card12 = Card.new(:diamond, 'Jack', 11)
+  card13 = Card.new(:heart, '8', 8)
+  card14 = Card.new(:diamond, '8', 8)
+  card15 = Card.new(:heart, '3', 3)
+  card16 = Card.new(:diamond, '2', 2)
 
   deck1 = Deck.new([card1, card2, card5, card8])
   deck2 = Deck.new([card3, card4, card6, card7])
+  deck3 = Deck.new([card9, card10, card13, card16])
+  deck4 = Deck.new([card12, card3, card14, card15])
 
   player1 = Player.new('Megan', deck1)
   player2 = Player.new('Aurora', deck2)
+  player3 = Player.new('Jean', deck3)
+  player4 = Player.new('Bobby', deck4)
 
   turn = Turn.new(player1, player2)
+  turn1 = Turn.new(player3, player4)
+
+  winner = turn.winner
 
   it 'exists' do
     expect(turn).to be_an_instance_of(Turn)
@@ -43,15 +58,25 @@ describe Turn do
     expect(turn.winner.name).to eq('Megan')
   end
 
-
   it 'either completely removes or shovels cards into spoils_of_war' do
     turn.pile_cards
     expect(turn.spoils_of_war).to eq([card1, card3])
   end
-binding.pry
-  xit 'gives spoils_of_war to winner' do
+
+  it 'gives spoils_of_war to winner' do
     turn.pile_cards
     turn.award_spoils(winner)
+    expect(player1.deck).to eq(deck1)
+    expect(player2.deck).to eq(deck2)
   end
+
+  it 'return type :mutually_assured_destruction and behaviors' do
+    expect(turn1.type).to eq(:mutually_assured_destruction)
+    expect(turn1.winner).to eq('No Winner')
+    turn.pile_cards
+    expect(turn1.spoils_of_war).to eq([])
+  end
+
+
 
 end
