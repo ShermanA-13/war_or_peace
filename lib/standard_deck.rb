96332612595a2b815b90a42655de4b3ae46904cf
Sherman Au <shermau13@gmail.com>
2022-02-04 11:09:45 -0700

    Successful rspec standard_deck.rb

---
+++ lib/standard_deck.rb
@@ -0,0 +1,27 @@
+require './lib/card'
+
+class StandardDeck
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+   @suits = [:diamond, :heart, :club, :spade]
+   @values_and_ranks = {values: ['2','3','4','5','6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'],
+                   ranks: [2, 3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14]}
+  end
+
+  def create_standard_deck
+    @suits.each do |suit|
+      13.times do |num|
+        value = @values_and_ranks[:values].at(num)
+        rank = @values_and_ranks[:ranks].at(num)
+        add_card(Card.new(suit.to_sym, value, rank))
+      end
+    end
+  end
+
+
+  def add_card(card)
+    @cards << card
+  end
+end
---
+++ spec/standard_deck_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require 'pry'
+require './lib/card'
+require './lib/standard_deck'
+
+describe StandardDeck do
+
+  new_deck = StandardDeck.new
+  new_deck.create_standard_deck
+
+  it 'exists' do
+    expect(new_deck).to be_an_instance_of(StandardDeck)
+  end
+
+  it 'knows those cards can be 4 different suits' do
+    expect(new_deck.cards.length).to eq(52)
+  end
+
+end
