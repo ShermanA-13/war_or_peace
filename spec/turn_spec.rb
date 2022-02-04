183c41d362d59798c899cecdd0b8540ce68d83a7
Sherman Au <shermau13@gmail.com>
2022-02-04 00:40:52 -0700

    Refactor and successsful rspec of type :mutally_assured_destruction and behaviors.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -14,19 +14,19 @@ describe Turn do
   card6 = Card.new(:diamond, 'Queen', 12)
   card7 = Card.new(:heart, '3', 3)
   card8 = Card.new(:diamond, '2', 2)
-  card1 = Card.new(:heart, 'Jack', 11)
-  card2 = Card.new(:heart, '10', 10)
-  card3 = Card.new(:heart, '9', 9)
-  card4 = Card.new(:diamond, 'Jack', 11)
-  card5 = Card.new(:heart, '8', 8)
-  card6 = Card.new(:diamond, '8', 8)
-  card7 = Card.new(:heart, '3', 3)
-  card8 = Card.new(:diamond, '2', 2)
+  card9 = Card.new(:heart, 'Jack', 11)
+  card10 = Card.new(:heart, '10', 10)
+  card11 = Card.new(:heart, '9', 9)
+  card12 = Card.new(:diamond, 'Jack', 11)
+  card13 = Card.new(:heart, '8', 8)
+  card14 = Card.new(:diamond, '8', 8)
+  card15 = Card.new(:heart, '3', 3)
+  card16 = Card.new(:diamond, '2', 2)
 
   deck1 = Deck.new([card1, card2, card5, card8])
   deck2 = Deck.new([card3, card4, card6, card7])
-  deck3 = Deck.new([card1, card2, card5, card8])
-  deck4 = Deck.new([card4, card3, card6, card7])
+  deck3 = Deck.new([card9, card10, card13, card16])
+  deck4 = Deck.new([card12, card11, card14, card15])
 
   player1 = Player.new('Megan', deck1)
   player2 = Player.new('Aurora', deck2)
@@ -70,12 +70,15 @@ describe Turn do
   end
 
   it 'returns type :mutually_assured_destruction and behaviors' do
+    turn1.type
+    turn1.winner
+    expect(turn1.winner).to eq("No Winner")
+
     turn1.pile_cards
-    turn1.award_spoils(winner)
-
-    expect(winner).to eq("No Winner")
-    expect(player3.deck.cards).to eq([card8])
-    expect(player4.deck.cards).to eq([card7])
+    turn1.spoils_of_war
+    player3.deck
+    player4.deck
+    expect(player3.deck.cards).to eq([card16])
+    expect(player4.deck.cards).to eq([card15])
   end
-
 end
